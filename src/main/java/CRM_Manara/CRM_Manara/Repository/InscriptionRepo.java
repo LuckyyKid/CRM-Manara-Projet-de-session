fbb9ad4ff7abcd17d09c4936e25076218db96a7c
2292583 <2292583@crosemont.qc.ca>
2026-02-23 16:44:36 -0500

    Extend inscription repo
    Add counts and animation lookup for deletes.

--- src/main/java/CRM_Manara/CRM_Manara/Repository/InscriptionRepo.java
+++ src/main/java/CRM_Manara/CRM_Manara/Repository/InscriptionRepo.java
@@ -14,4 +14,14 @@ public interface InscriptionRepo extends JpaRepository<Inscription, Long> {
 
     @Query("SELECT i FROM Inscription i JOIN i.enfant e WHERE e.parent.id = :parentId")
     List<Inscription> findByParentId(@Param("parentId") Long parentId);
+
+    @Query("SELECT i FROM Inscription i WHERE i.animation.id = :animationId")
+    List<Inscription> findByAnimationId(@Param("animationId") Long animationId);
+
+    long countByAnimationId(Long animationId);
+
+    long countByEnfantId(Long enfantId);
+
+    @Query("SELECT COUNT(i) FROM Inscription i WHERE i.animation.activity.id = :activityId")
+    long countByActivityId(@Param("activityId") Long activityId);
 }
