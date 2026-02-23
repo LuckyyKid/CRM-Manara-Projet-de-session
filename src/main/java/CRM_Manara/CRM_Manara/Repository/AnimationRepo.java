c57565fcb4dfd4b3f1b7f16b64c38accbaa65329
2292583 <2292583@crosemont.qc.ca>
2026-02-22 23:13:25 -0500

    Add animation repo
    Query animations by activity.

---
+++ src/main/java/CRM_Manara/CRM_Manara/Repository/AnimationRepo.java
@@ -0,0 +1,14 @@
+package CRM_Manara.CRM_Manara.Repository;
+
+import CRM_Manara.CRM_Manara.Model.Entity.Animation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AnimationRepo extends JpaRepository<Animation, Long> {
+
+    @Query("SELECT a FROM Animation a WHERE a.activity.id = :activityId")
+    List<Animation> findByActivityId(@Param("activityId") Long activityId);
+}
