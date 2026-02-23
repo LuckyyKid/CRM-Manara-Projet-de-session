7be7704a03706124fa46d6d0b8a4e77e14db4751
2292583 <2292583@crosemont.qc.ca>
2026-02-22 23:13:29 -0500

    Add animateur repo
    JPA access for animateurs.

---
+++ src/main/java/CRM_Manara/CRM_Manara/Repository/AnimateurRepo.java
@@ -0,0 +1,7 @@
+package CRM_Manara.CRM_Manara.Repository;
+
+import CRM_Manara.CRM_Manara.Model.Entity.Animateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimateurRepo extends JpaRepository<Animateur, Long> {
+}
