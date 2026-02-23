535179bb33ede9a7179d8b3e36871f061c01ac11
2292583 <2292583@crosemont.qc.ca>
2026-02-23 15:55:45 -0500

    Update test: EnfantRepoTest.java
    Switch to SpringBootTest for compatibility.

--- src/test/java/CRM_Manara/CRM_Manara/Repository/EnfantRepoTest.java
+++ src/test/java/CRM_Manara/CRM_Manara/Repository/EnfantRepoTest.java
@@ -7,14 +7,14 @@ import CRM_Manara.CRM_Manara.Model.Entity.User;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.sql.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 @ActiveProfiles("test")
 class EnfantRepoTest {
 
