437a8753a98fd223d256ecc6b49c0e3fe266e863
2292583 <2292583@crosemont.qc.ca>
2026-02-22 23:12:38 -0500

    Clean auth controller
    Minor adjustments for login route.

--- src/main/java/CRM_Manara/CRM_Manara/Controller/authController.java
+++ src/main/java/CRM_Manara/CRM_Manara/Controller/authController.java
@@ -1,7 +1,5 @@
 package CRM_Manara.CRM_Manara.Controller;
 
-import CRM_Manara.CRM_Manara.Model.Entity.Service.parentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
